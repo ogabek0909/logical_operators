a3f0acb8375844957651338376caa3da793fda65
ogabek <norpulatovogabek42@gmail.com>
2022-06-29 18:07:38 +0500

    task12

--- bin/logical_operators12.dart
+++ bin/logical_operators12.dart
@@ -8,4 +8,13 @@ Given a two-digit integer a,  check the following statement "All digits of the n
         bool: answer
 */
 
-void main() {}
+bool func(a) {
+  a > 9 && a < 100;
+  int w = a ~/ 10;
+  int q = a % 10;
+  return w == q;
+}
+
+void main() {
+  print(func(69));
+}
