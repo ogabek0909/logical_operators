1b8b07cb5d3b2f96de5ea0e513c5fc0fd80af668
ogabek <norpulatovogabek42@gmail.com>
2022-06-29 18:14:08 +0500

    task13

--- bin/logical_operators13.dart
+++ bin/logical_operators13.dart
@@ -7,5 +7,16 @@ Given a two-digit integer a,  check the following statement "All digits sum is e
     Returns:
         bool: answer
 */
+bool func(a) {
+  a > 99 && a < 1000;
+  int s = a % 10;
+  a = a ~/ 10;
+  int q = a % 10;
+  a = a ~/ 10;
+  int w = a % 10;
+  return 1 - s % 2 == 1 && 1 - q % 2 == 1 && 1 - w % 2 == 1;
+}
 
-void main() {}
+void main() {
+  print(func(246));
+}
